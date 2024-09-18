c6009a7c474c3184089f7a21fbb3ef49f31eaeb8
JonaIbarra <ei9063584@gmail.com>
2024-09-18 14:14:01 -0600

    Practica 06
    Cambios en el Readme para agregar la practica 6

--- DartPad/Ext04_Mapas.dart
+++ DartPad/Ext04_Mapas.dart
@@ -1,70 +1,54 @@
-void main() {
-  
-  final Map <String, dynamic > pokemon = {
-    'name' : 'Ditto',
-    'hp' : 100,
+void main()
+{
+  final pokemon = {
+    'Name': 'Pikachu',
+    'HP': 100, 
     'isAlive': true,
-    'abilities': ['Impact Trueno', 'Cola de Hierro', 'Rapido'],
-    'sprites' : 
+    'abilities': ['Impact Trueno', 'Cola de hierro', 'Rápido'],
+    'sprites': 
     {
       1: "pikachu/front.png",
-      2: "pikachu/back.png"
-    }     
-  };
-  
-  final Map<String, dynamic> trainer ={
-    'Name' : 'Ash Ketchup',
-    'Glender': 'Male',
-    'Age' : 12,
-    'isGymnasiumLeader' : false
+      2: "pikachu/back.png",
+    }
   };
 
+  final Map<String, dynamic>trainer = {
+    'Name': 'Ash Ketchup',
+    'Gender': 'Male',
+    'Age': 12,
+    'isGymnasiumLeader': false
+  };
 
-  final pokemons = {
+  final pokemons=
+  {
     1: "Pikachu",
     2: "Charmander",
     3: "Squirtle",
     4: "Pidgeot"
   };
+  
 
-
-  print("""
-  Los datos del Pokemon son usando el Mapa: 
-  -----------------------------------
-  Pokemon = $pokemon
+  print(""" 
+  Los datos del pokemon usando el Mapa son:
+  -------------------------------------------------
+  Pokemon= $pokemon
   Trainer = $trainer
-  Pokemons = $pokemons
-  -----------""");
-  print("****************************************************");
-
-
-
-
-
-// print ("""
-
-//   // Reto 01: Obtener el valor independiente de las imagenes de Front y Back del Pokemon
-
-//   // Front: ${(pokemon[sprites])[1]}
-//   // Back: ${(pokemon[sprites])[2]}
-
-
-// // Dado que la solucion ya esta obsoleta lo que queda es crear una nueva variable
-
-
-
-// """)
-
-  print("""
-  Accediendo a las propiedades del pokemon usando los braquets/ 
-  corchetes []: 
-  -----------------------------------
-  Nombre = ${pokemon['name']}
-  HP = ${pokemon['hp']}
-  Vivo = "${pokemon['isAlive']}
-  Sprites = "${pokemon['sprites']}
-
-  Front = "${pokemon['tmp_sprites'][1]}
-  Back = "${pokemon['tmp_sprites'][2]}
+  Pokemons= $pokemons
   """);
+  final tmp_sprites = pokemon ['sprites'] as Map<int, String>;
+  
+  print("********");
+  print(""" 
+  Accediendo a las propiedades del pokemon usando los braquets/corchetes []:
+  ----------------------------------------------------------------------------
+  Nombre = ${pokemon['Name']}
+  HP = ${pokemon['HP']}
+  Vivo = ${pokemon['isAlive']}
+  Sprites = ${pokemon['sprites']}
+  ------------------------------------------------------------------------------------------
+  
+  Back: ${tmp_sprites[2]}
+  Front: ${tmp_sprites[1]}
+  """);
+ 
 }
