145266a5c646d4479e77f928c97055fe23c494f1
JonaIbarra <ei9063584@gmail.com>
2024-09-23 17:49:25 -0600

    Practica 07
    Agregacion del ejecicio 05 Listas, Conjuntos, Iterables

---
+++ DartPad/Ext05_Listas_Conjuntos_Iterables.dart
@@ -0,0 +1,18 @@
+void main() {
+  
+  
+  // La declaración de una lista exta limitada por corchetes (brackets) y cada elemento de ser individual y puede repetirse.
+  
+  final numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
+  print(""numeros);
+  
+  
+  
+
+
+
+
+
+  
+}
