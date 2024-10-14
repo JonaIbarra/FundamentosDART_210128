cd604db2a57d0bc8f8c3468c01f4dce1bebe67fc
JonaIbarra <ei9063584@gmail.com>
2024-10-14 15:41:49 -0600

    Practica 12
    Se agrego el documento Ext10_Aserciones.dart

---
+++ DartPad/Ext10_Aserciones.dart
@@ -0,0 +1,30 @@
+void main(){
+  final mySquqre = Square ( side:10);
+  print("""El lado del cuadrado es de: ${mySquare._side}
+  y su area es de: ${mySquare.area} """);
+  mySquare.side = -5;
+  print("""El lado del cuadrado es de: ${mySquare._side}
+  y su area es de  ${mySquare.area} """);
+}
+
+class Square{
+  double _side ;// side * side 
+
+  Square({required double side })
+  : assert(x >= 0, 'El valor debe de sere mayor a 0'),
+    :_side = side;
+  double get area {
+    return _side * _side ;
+    
+  }
+
+  set side( double value ) {
+    print ( 'El valor asignado al lado $value' ) ;
+    if ( value < 0 ) throw 'El valor debe de ser mayor a 0.';
+    _side = value;
+  }
+
+  double calculateArea(){
+    return _side * _side;
+  }
+}
